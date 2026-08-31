12984d2025c6feafe983850465db23dd4cb965fe
kdoss0157-ctrl <kdoss0157@gmail.com>
2026-08-31 11:29:42 +0530

    Update answers.sql

--- starter/answers.sql
+++ starter/answers.sql
@@ -1,1 +1,21 @@
+CREATE DATABASE COLLEGE;
+USE COLLEGE;
+CREATE TABLE Student (
+    StudentID INT PRIMARY KEY,
+    StudentName VARCHAR(20) NOT NULL UNIQUE,
+    DOB DATE NOT NULL,
+    Gender VARCHAR(10) NOT NULL,
+    DepartmentID INT NOT NULL
+);
+INSERT Student values("1001","kalidoss","2007-01-12","female","101");
+desc Student;
+select*from Student;
+
+ALTER TABLE Student
+ADD Email VARCHAR(30);
+
+ALTER TABLE Student
+ADD PhoneNumber BIGINT;
+
+DESCRIBE Student;
 
